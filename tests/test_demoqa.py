20a5a0fcb7864cd98d41332cb827de23b05a4710
Irina Lavrenteva <lis201002@gmail.com>
2025-10-29 00:30:46 +0300

    project without allure

---
+++ tests/test_demoqa.py
@@ -0,0 +1,11 @@
+from data import users
+from pages.registration_page import RegistrationPage
+
+
+def test_demo_qa_form(browser_setup):
+    registration_page = RegistrationPage()
+    student = users.student
+
+    registration_page.open()
+    registration_page.register(student)
+    registration_page.should_have_registered_user(student)
