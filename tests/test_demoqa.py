01a0cdf19716cce87b8dfed4dbfbdff1d96b6456
Irina Lavrenteva <lis201002@gmail.com>
2025-11-03 21:12:35 +0300

    project with allure

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,11 +1,16 @@
+import allure
+
 from data import users
 from pages.registration_page import RegistrationPage
 
 
+@allure.title("Successful fill form")
 def test_demo_qa_form(browser_setup):
     registration_page = RegistrationPage()
     student = users.student
-
-    registration_page.open()
-    registration_page.register(student)
-    registration_page.should_have_registered_user(student)
+    with allure.step("Open registrations form"):
+        registration_page.open()
+    with allure.step("Fill form"):
+        registration_page.register(student)
+    with allure.step("Check form results"):
+        registration_page.should_have_registered_user(student)
