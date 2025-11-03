4f23e08378845fae12e5b6e7ec908b7ab94b3877
Irina Lavrenteva <lis201002@gmail.com>
2025-11-03 23:34:09 +0300

    add login password env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,25 @@
+import os
+
 import pytest
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
 
+DEFAULT_BROWSER_VERSION = "128.0"
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='module')
 def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = "https://demoqa.com"
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -21,7 +31,7 @@ def browser_setup():
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
